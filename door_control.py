09dbf564e2ed8d2c0eb5ca210e73b3100cb5dceb
Mike Turbutt <mike.turbutt@dotter.me>
2017-06-28 23:46:09 +0100

    major refactor

--- door_control.py
+++ door_control.py
@@ -11,13 +11,20 @@ import httplib, urllib  # for Push Notifications
 pushover_token = 'aZ48abAEXfJouw9p3ZHaXPtrgo1URM'
 pushover_user = 'u585xQwjtPfKEQdjFkdC243J8uryyk'
 
+RunningTime = 0
+DoorStatus = 'unknown'
+TimeStart = time.clock()
+
 # Setting up Board GPIO Pins
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.OUT)  # motor
 GPIO.setup(22, GPIO.OUT)  # motor
 GPIO.setup(16, GPIO.IN)  # Open
-GPIO.setup(12, GPIO.IN)  # Locked
+GPIO.setup(12, GPIO.IN)  # Closed
 
+def getElapsedTime():
+    time.sleep(1)
+    return time.clock() - TimeStart
 
 # Clean kill of script function (Stops Motor, cleans GPIO)
 def Safe_Kill():
@@ -25,21 +32,41 @@ def Safe_Kill():
     GPIO.output(18, False)
     GPIO.output(22, False)
     GPIO.cleanup()
+    time.sleep(1)
     return('Motors shutdown, GPIO cleaned')
 
+def isOpen():
+    return GPIO.input(16) == 0
+    
+def isClosed():    
+    return GPIO.input(12) == 0
+ 
+def driveOpen():
+    GPIO.output(18, True)
+    GPIO.output(22, False)
 
-def PushOver(message):
+def driveClosed():
+    GPIO.output(18, False)
+    GPIO.output(22, True)
+
+if isClosed(): DoorStatus = 'Closed'
+elif isOpen(): DoorStatus = 'open'
+
+def PushOver(message, sound):
+    Safe_Kill()
+    print 'Sending Pushover', message, sound
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
          urllib.urlencode({
              "token": pushover_token,
              "user": pushover_user,
              "message": message,
-             "sound": "bugle",
+             "sound": sound,
              "timestamp": datetime.datetime,
          }), {"Content-type": "application/x-www-form-urlencoded"})
     response = conn.getresponse()
     print response.status
+    sys.exit(0)
 
 # Argument controller
 if len(sys.argv) > 3:  # Tests if you've entered too many arguments
@@ -67,118 +94,56 @@ if len(sys.argv) > 1:  # Argument for door action
 
 if len(sys.argv) == 3:
     print 'Forcing door to', str(sys.argv[1]), 'for', str(sys.argv[2]), 'seconds'
-    Door_Action = sys.argv[1]
-    Door_Time = int(sys.argv[2])
+    DoorAction = sys.argv[1]
+    DoorTimeout = int(sys.argv[2])
 if len(sys.argv) == 2:
     print 'Forcing door to ', str(sys.argv[1])
-    Door_Action = sys.argv[1]
-    Door_Time = 39  # This is a safety time
+    DoorAction = sys.argv[1]
+    DoorTimeout = 39  # This is a safety time
 if len(sys.argv) == 1:
-    Door_Action = 'default'  # Will reverse door state
-    Door_Time = 39  # This is a safety time
+    DoorAction = 'default'  # Will reverse door state
+    DoorTimeout = 39  # This is a safety time
 
-print 'Door_Action is', Door_Action
-print 'Door_Time is', Door_Time
+print 'DoorAction is', DoorAction
+print 'DoorTimeout is', DoorTimeout
+print 'DoorStatus is', DoorStatus
 
-# Start door!
+# TODO try and except 
 
-# def DoorControl():
-TimeStart = time.clock()
-runTime = 0
-# Check door status from Magnets
-BottomHall = GPIO.input(12)
-TopHall = GPIO.input(16)
-if BottomHall == 0: print 'Door is locked'
-if TopHall == 0: print 'Door is open'
-if BottomHall == 1: print 'No magnet sensed on lock'
-if TopHall == 1: print 'No magnet sensed top'
+if DoorAction == 'status':
+    PushOver(DoorStatus, 'Bike')
+elif DoorAction == 'open':
+    while RunningTime < DoorTimeout:
+        driveOpen()
+        RunningTime = getElapsedTime()
+        if isOpen(): PushOver('Coop opened successfully in ' + str(RunningTime) + ' secs :)', 'Bugle')
+    PushOver('Coop open FAILED! :(', 'Siren')
+elif DoorAction == 'close':
+    while RunningTime < DoorTimeout:
+        driveClosed()
+        RunningTime = getElapsedTime()
+        if isClosed(): PushOver('Coop closed successfully in ' + str(RunningTime) + ' secs :)', 'Bugle')
+    PushOver('Coop close FAILED! :(', 'Siren')
+elif DoorAction != 'default':
+    PushOver('Invalid DoorAction of: ' + DoorAction, 'Run')
+elif isClosed() and isOpen():
+    PushOver('Invalid DoorStatus (both): ' + DoorStatus, 'Run')
+elif not isClosed() and not isOpen():
+    PushOver('Invalid DoorStatus (neither): ' + DoorStatus, 'Run')
+elif isClosed():
+    print 'The door is closed and so being opened!'
+    while RunningTime < DoorTimeout:
+        driveOpen()
+        RunningTime = getElapsedTime()
+        if isOpen(): PushOver('Coop opened successfully in ' + str(RunningTime) + ' secs :)', 'Bugle') 
+    PushOver('Coop open FAILED! :(', 'Siren')    
+elif isOpen():
+    print 'The door is open and so going down!'
+    while RunningTime < DoorTimeout:
+        driveClosed()
+        RunningTime = getElapsedTime()
+        if isClosed(): PushOver('Coop closed successfully ' + str(RunningTime) + ' secs :)', 'Bugle') 
+    PushOver('Coop close FAILED! :(', 'Siren')
 
-if Door_Action == 'open':  # Door is locked
-    print 'The door is locked!'
-    print 'The door is going up!'
-    while TopHall == 1 and runTime < Door_Time:
-        GPIO.output(18, True)
-        GPIO.output(22, False)
-        TopHall = GPIO.input(16)
-        runTime = time.clock() - TimeStart
-    if 39 >= runTime:
-        print 'Something went wrong, go check the door!',str(runTime)
-        message = 'Coop open FAILED!'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-    if TopHall == 0:
-        time.sleep(1)
-        print 'Door is open!'
-        message = 'Coop opened successfully in ' + str(runTime) + ' secs.'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-elif Door_Action == 'close':  # Door is open
-    print 'The door is open!'
-    print 'The door is going down!'
-    while BottomHall == 1 and runTime < Door_Time:
-        GPIO.output(18, False)
-        GPIO.output(22, True)
-        BottomHall = GPIO.input(12)
-        runTime = time.clock() - TimeStart
-    if 39 >= runTime:
-        print 'Something went wrong, go check the door!',str(runTime)
-        message = 'Coop close FAILED!'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-    if BottomHall == 0:
-        time.sleep(1)
-        print 'Door is locked!'
-        message = 'Coop closed successfully ' + str(runTime) + ' secs.'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-elif BottomHall == 0:  # Door_Action is 'default' and Door is locked
-    print 'The door is locked!'
-    print 'The door is going up!'
-    while TopHall == 1 and runTime < Door_Time:
-        GPIO.output(18, True)
-        GPIO.output(22, False)
-        TopHall = GPIO.input(16)
-        runTime = time.clock() - TimeStart
-    if 39 >= runTime:
-        print 'Something went wrong, go check the door!',str(runTime)
-        message = 'Coop open FAILED!'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-    if TopHall == 0:
-        time.sleep(1)
-        print 'Door is open!'
-        message = 'Coop opened successfully ' + str(runTime) + ' secs.'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-elif TopHall == 0:  # Door_Action is 'default' and Door is open
-    print 'The door is open!'
-    print 'The door is going down!'
-    while BottomHall == 1 and runTime < Door_Time:
-        GPIO.output(18, False)
-        GPIO.output(22, True)
-        BottomHall = GPIO.input(12)
-        runTime = time.clock() - TimeStart
-    if 39 >= runTime:
-        print 'Something went wrong, go check the door!',str(runTime)
-        message = 'Coop close FAILED!'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
-    if BottomHall == 0:
-        time.sleep(1)
-        print 'Door is locked!'
-        message = 'Coop closed successfully ' + str(runTime) + ' secs.'
-        Safe_Kill()
-        PushOver(message)
-        sys.exit(0)
+PushOver('WTF is happening?! :o', 'Run') 
 
-runTime = time.clock() - TimeStart
-print 'Total Time: ' + str(runTime)
-Safe_Kill()
-sys.exit(0)
